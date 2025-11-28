8c42fbc4d0d4da2dbce1c15a8231580ec5df6240
Ahmed Hashim <devahmedswe@gmail.com>
2025-11-28 06:32:13 +0200

    Complete Form with states

--- src/LoanForm.jsx
+++ src/LoanForm.jsx
@@ -1,65 +1,91 @@
+
+// import OverlayApp from './OverlayApp'
+import { useState } from "react";
+
 export default function LoanForm() {
 
+  const [loanInputs, setLoanInputs] = useState(
+    {
+      name:"",
+      phone:"",
+      age:"",
+      isEmployee:"false", 
+      salary:""
+
+    }
+  )
+
   return (
-    <div className="loan-container">
-      <h1>Loan Request</h1>
-      {/* name - phone - age Check employee - salary   */}
-      <form 
-      // onSubmit={} 
-      style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-        <div className="loan-input">
-          <label>Full Name</label>
+    <>
+      <div className="loan-container">
+        <h1>Loan Request</h1>
+        <form 
+        // onSubmit={} 
+        style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+          <div className="loan-input">
+            <label>Full Name</label>
 
-          <input
-            type="text"
-            // value={}
-            // onChange={}
-            placeholder="What is your full name: "
-          />
-        </div>
-
-        <div className="loan-input">
-          <label>Age</label>
-          <input
-            type="number"
-            // value={}
-            // onChange={}
-            placeholder="What is your age: "
-          />
+            <input
+              type="text"
+              value={loanInputs.name}
+              onChange={(e)=> {setLoanInputs({...loanInputs,name:e.target.value})}}
+              placeholder="What is your full name: "
+            />
           </div>
 
-        <div className="loan-input">
-          <label>Phone</label>
-          <input
-            type="number"
-            // value={}
-            // onChange={}
-            placeholder="What is your phone number: "
-          />
-        </div>
-
-        <div className="loan-input">
-          <label>Salary</label>
-          <input
-            type="number"
-            // value={}
-            // onChange={}
-            placeholder="What is your phone number: "
-          />
-        </div>
-
-        <div className="checkbox-container">
-          <label>
-          <input
-            type="checkbox"
-            // checked={}
-            // onChange={}
+          <div className="loan-input">
+            <label>Phone</label>
+            <input
+              type="number"
+              value={loanInputs.phone}
+              onChange={(e)=> {setLoanInputs({...loanInputs,phone:e.target.value})}}
+              placeholder="What is your phone number: "
             />
-              I,m an Employee </label>
-        </div> 
+          </div>
 
-        <button type="submit">Submit Request</button>
-      </form>
-    </div>
+
+          <div className="loan-input">
+            <label>Age</label>
+            <input
+              type="number"
+              value={loanInputs.age}
+              onChange={(e)=> {setLoanInputs({...loanInputs,age:e.target.value})}}
+              placeholder="What is your age: "
+            />
+            </div>
+
+          <div className="loan-input">
+            <label>Salary</label>
+            <input
+              type="number"
+              value={loanInputs.salary}
+              onChange={(e)=> {setLoanInputs({...loanInputs,salary:e.target.value})}}
+
+              placeholder="What is your phone number: "
+            />
+          </div>
+
+          <div className="checkbox-container">
+            <label>
+            <input
+              type="checkbox"
+              checked={!!loanInputs.isEmployee}
+              onChange={(e)=> {setLoanInputs({...loanInputs,isEmployee:e.target.checked})}}
+
+              />
+                I,m an Employee </label>
+          </div> 
+
+          <button 
+            disabled={!(loanInputs.name && loanInputs.age && loanInputs.salary)}
+            type="submit">Submit Request</button>
+        </form>
+      </div>
+      
+      <div>
+        {/* <OverlayApp/> */}
+      </div>
+    </>
+
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LoanForm from './LoanForm'
-import OverlayApp from './OverlayApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LoanForm/>
-    <OverlayApp/>
   </StrictMode>,
 )
